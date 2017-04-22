53d2ad2a86db5092eae38a6d78fc043afd25da5a
bhavik kheni <kheni.bhavik@gmail.com>
2017-04-22 23:04:15 +0530

    added contact

--- react-redux/react-redux-example/src/containers/contactApp.js
+++ react-redux/react-redux-example/src/containers/contactApp.js
@@ -3,8 +3,9 @@ import { Field, reduxForm, getFormValues } from 'redux-form';
 import { connect } from 'react-redux';
 import * as action from '../actions/actions';
 
+import userImage from '../../public/images.jpg';
 import classNames from 'classnames';
-import { Button, Form, List, Image, Card } from 'semantic-ui-react';
+import { Button, Form, List, Image, Card, Segment } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 function validate(values) {
@@ -62,43 +63,58 @@ export class ContactForm extends Component {
         const { users, handleSubmit } = this.props;
         return (
             <div>
-                <div style={{display:'flex',justifyContent:'center'}}>
-                <Card>
-                    <Card.Content>
-                        <Card.Header>
-                            Redux Form
+                <div style={{ display: 'flex', justifyContent: 'center', marginTop: 40 }}>
+                    <Card>
+                        <Card.Content>
+                            <Card.Header>
+                                Redux Form
                         </Card.Header>
 
-                        <Card.Description>
-                            <Form onSubmit={handleSubmit(this.props.testSave || this.onSave)} style={{ marginLeft: 50, marginTop: 10, marginBottom: 30 }}>
-                                <div>
-                                    <Form.Group widths='equal'>
-                                        <Field name="firstName" component={this.renderInput}
-                                            type="text" placeholder="First Name" />
-                                    </Form.Group>
-                                </div>
-                                <div>
-                                    <Form.Group widths='equal'>
-                                        <Field name="lastName" component={this.renderInput} type="text" placeholder="Last Name" />
-                                    </Form.Group>
-                                </div>
-                                <div>
-                                    <Form.Group widths='equal'>
-                                        <Field name="email" component={this.renderInput} type="email" placeholder="Email" />
-                                    </Form.Group>
-                                </div>
-                                <div>
-
-                                </div>
-                                <div>
+                            <Card.Description>
+                                <Form onSubmit={handleSubmit(this.props.testSave || this.onSave)} style={{ marginLeft: 50, marginTop: 10, marginBottom: 30 }}>
                                     <div>
-                                        <Button basic color='blue' id="submitButton" type="submit" ref={(c) => { this.submitButton = c; }}>Submit</Button>
+                                        <Form.Group widths='equal'>
+                                            <Field name="firstName" component={this.renderInput}
+                                                type="text" placeholder="First Name" />
+                                        </Form.Group>
                                     </div>
-                                </div>
-                            </Form>
-                        </Card.Description>
-                    </Card.Content>
-                </Card>
+                                    <div>
+                                        <Form.Group widths='equal'>
+                                            <Field name="lastName" component={this.renderInput} type="text" placeholder="Last Name" />
+                                        </Form.Group>
+                                    </div>
+                                    <div>
+                                        <Form.Group widths='equal'>
+                                            <Field name="email" component={this.renderInput} type="email" placeholder="Email" />
+                                        </Form.Group>
+                                    </div>
+                                    <div>
+
+                                    </div>
+                                    <div>
+                                        <div>
+                                            <Button basic color='blue' id="submitButton" type="submit" ref={(c) => { this.submitButton = c; }}>Submit</Button>
+                                        </div>
+                                    </div>
+                                </Form>
+                            </Card.Description>
+                        </Card.Content>
+                    </Card>
+                </div>
+                <div style={{ display: 'flex', marginTop: '2%' }}>
+                        <List celled>
+                            { 
+                                users && users.map((user, i) => (
+                                    <List.Item key={i} >
+                                        <Image avatar src={userImage} />
+                                        <List.Content>
+                                            <List.Header as='a' style={{ color: 'white' }}>{user.user.firstName} &nbsp; {user.user.lastName}</List.Header>
+                                            <List.Description>My Email is: <a><b>{user.user.email}</b></a></List.Description>
+                                        </List.Content>
+                                    </List.Item>
+                                ))
+                            }
+                        </List>
                 </div>
             </div>
         );
